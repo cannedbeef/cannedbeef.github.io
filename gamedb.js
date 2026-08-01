9c75c6f621af6ae4536ffebb65eda874b905fb92
cannedbeef <cannedbeef255@proton.me>
2026-08-01 20:33:09 +1000

    adjust credits

--- gamedb.js
+++ gamedb.js
@@ -4,7 +4,7 @@ window.games = {
 	"slope": {
 		"name": "Slope",
 		"tags": ["classic", "infinite"],
-		"credits": "Y8 games",
+		"credits": "Y8 games (hosted by slopenexus on github)",
 		"alt-links": ["https://harshulgoon.github.io/iframes/slope.html", "https://slope2-game.com/slope-unblocked", "https://mathplayzone.com/slope/", "https://www.y8.com/games/slope", "https://slope-play.github.io/"],
 		"description": "this is like the default unblocked game. roll down a hill and try not to die.",
 		"iframe": "https://slopenexus.github.io/games/19-04v2.1/index.html",
@@ -14,7 +14,7 @@ window.games = {
 	"run3": {
 		"name": "Run 3",
 		"tags": ["classic", "infinite", "story_basic"],
-		"credits": "Joseph Cloutier",
+		"credits": "player_03 (hosted by lekug on github)",
 		"alt-links": ["https://dnrweqffuwjtx.cloudfront.net/run-3/", "https://lekug.github.io/tn6pS9dCf37xAhkJv/", "https://run3-unblock.github.io/", "https://player03.com/run/3/beta/", "https://www.hoodamath.com/games/run3.html", "https://run-3.bitbucket.io/", "https://run3free.github.io/"],
 		"description": "you're an alien running through space tunnels. i think it has a story? idk though.",
 		"iframe": "https://lekug.github.io/tn6pS9dCf37xAhkJv/",
@@ -24,7 +24,7 @@ window.games = {
 	"stickmanhook": {
 		"name": "Stickman Hook",
 		"tags": ["classic", "infinite"],
-		"credits": "madbox (hosted by anonymous gitlab user)",
+		"credits": "Madbox (hosted by stickmanhook on gitlab)",
 		"alt-links": ["https://harshulgoon.github.io/iframes/stickman.html", "https://stickman-hook.bitbucket.io/", "https://stickmanhook.bitbucket.io/", "https://stickmanhook-unblocked.github.io/", "https://poki.com/en/g/stickman-hook", "https://www.zgames.io/game/stickman-hook-unblocked-free-browser-game", "https://mathplayzone.com/stickman-hook/"],
 		"description": "swing through (technically) infinite levels, you can go stupidly fast if you spam",
 		"iframe": "https://stickmanhook.gitlab.io/file/",
