afe5e7782e7b703d1a9b47b4d7990a0de6efa1f7
cannedbeef <cannedbeef255@proton.me>
2026-08-01 20:35:01 +1000

    add 2d and 3d tags

--- gamedb.js
+++ gamedb.js
@@ -3,7 +3,7 @@
 window.games = {
 	"slope": {
 		"name": "Slope",
-		"tags": ["classic", "infinite"],
+		"tags": ["3d", "classic", "infinite"],
 		"credits": "Y8 games (hosted by slopenexus on github)",
 		"alt-links": ["https://harshulgoon.github.io/iframes/slope.html", "https://slope2-game.com/slope-unblocked", "https://mathplayzone.com/slope/", "https://www.y8.com/games/slope", "https://slope-play.github.io/"],
 		"description": "this is like the default unblocked game. roll down a hill and try not to die.",
@@ -13,7 +13,7 @@ window.games = {
 
 	"run3": {
 		"name": "Run 3",
-		"tags": ["classic", "infinite", "story_basic"],
+		"tags": ["3d", "classic", "infinite", "story_basic"],
 		"credits": "player_03 (hosted by lekug on github)",
 		"alt-links": ["https://dnrweqffuwjtx.cloudfront.net/run-3/", "https://lekug.github.io/tn6pS9dCf37xAhkJv/", "https://run3-unblock.github.io/", "https://player03.com/run/3/beta/", "https://www.hoodamath.com/games/run3.html", "https://run-3.bitbucket.io/", "https://run3free.github.io/"],
 		"description": "you're an alien running through space tunnels. i think it has a story? idk though.",
@@ -23,7 +23,7 @@ window.games = {
 
 	"stickmanhook": {
 		"name": "Stickman Hook",
-		"tags": ["classic", "infinite"],
+		"tags": ["3d", "classic", "infinite"],
 		"credits": "Madbox (hosted by stickmanhook on gitlab)",
 		"alt-links": ["https://harshulgoon.github.io/iframes/stickman.html", "https://stickman-hook.bitbucket.io/", "https://stickmanhook.bitbucket.io/", "https://stickmanhook-unblocked.github.io/", "https://poki.com/en/g/stickman-hook", "https://www.zgames.io/game/stickman-hook-unblocked-free-browser-game", "https://mathplayzone.com/stickman-hook/"],
 		"description": "swing through (technically) infinite levels, you can go stupidly fast if you spam",
